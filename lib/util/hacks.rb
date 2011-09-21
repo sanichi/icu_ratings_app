5892f6eb8ee4274d1cbbb6a32ecd7a1c076a5b35
Mark Orr <mark.j.l.orr@googlemail.com>
2011-09-21 21:55:03 +0100

    Tweak to comment

--- lib/util/hacks.rb
+++ lib/util/hacks.rb
@@ -2,18 +2,18 @@ require 'mime/types'
 
 module Util
   class Hacks
+    # Need to persuade MIME::Types to regard "tab" (a popular ending for Krause files)
+    # as signaling a text/plain file type so that file uploads in Capybara will work.
+    # The following is ugly and it causes a warning to stderr, but I don't see a better way yet.
+    # http://stackoverflow.com/questions/7477517/how-to-add-to-the-extensions-for-an-existing-type-in-rubys-mimetypes/7477635.
     def self.fix_mime_types
-      # Prepare to hide a warning message.
       orig_stderr = $stderr
       $stderr = File.new('/dev/null', 'w')
 
-      # Don't see a better way to do this.
-      # http://stackoverflow.com/questions/7477517/how-to-add-to-the-extensions-for-an-existing-type-in-rubys-mimetypes/7477635.
       text_plain = MIME::Types['text/plain'].first.to_hash
       text_plain['Extensions'].push('tab')
       MIME::Types.add(MIME::Type.from_hash(text_plain))
 
-      # Restore the real stderr.
       $stderr = orig_stderr
     end
   end
