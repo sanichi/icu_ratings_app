2839a24025bfe55c0d69bfb8ecf37b11b658a826
Mark Orr <mark.j.l.orr@googlemail.com>
2012-02-03 17:25:20 +0000

    Added information about environment variables to overview page

--- app/presenters/pages/overview.rb
+++ app/presenters/pages/overview.rb
@@ -60,5 +60,15 @@ module Pages
       @p.each_key { |d| @p[d][:icon] = "problems" if @p[d][:count] > 0 }
       @p
     end
+
+    def environment
+      return @e if @e
+      @e = ENV.keys.sort.inject({}) do |h, k|
+        v = ENV[k].dup
+        v = v.split(/:/).map{ |p| p == "" ? "(blank)" : p }.join("<br/>").html_safe if k.match(/PATH/)
+        h[k] = v
+        h
+      end
+    end
   end
 end
