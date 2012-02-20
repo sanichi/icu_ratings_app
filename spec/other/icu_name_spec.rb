4fb9636b75585e96378fa69f0c85cbacf54adc3c
Mark Orr <mark.j.l.orr@googlemail.com>
2012-02-20 16:33:04 +0000

    New name alternative

--- spec/other/icu_name_spec.rb
+++ spec/other/icu_name_spec.rb
@@ -1,10 +1,12 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 module ICU
   describe Name do
     it "should handle new additions to alternative_first_names" do
-      Name.new('Douglas', 'McCann').match('Dougie', 'McCann').should be_true
-      Name.new('Edward', 'Walsh').match('Ned', 'Walsh').should be_true
+      Name.new("Douglas", "McCann").match("Dougie", "McCann").should be_true
+      Name.new("Edward", "Walsh").match("Ned", "Walsh").should be_true
+      Name.new("Michael", "Morgan").match("Míchéal", "Morgan", chars: "US-ASCII").should be_true
     end
   end
 end
