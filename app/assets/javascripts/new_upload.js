916870a03e0fbec80b204b9385a0acc878e3e5f6
Mark Orr <mark.j.l.orr@googlemail.com>
2012-06-17 14:47:33 +0100

    Simple javascript form checker for new uploads

--- app/assets/javascripts/new_upload.js
+++ app/assets/javascripts/new_upload.js
@@ -22,8 +22,21 @@ function toggle_help_sections(selected) {
   $("#upload_format option").each(function() { $("#help ." + this.value).hide() });
   $("#help ." + selected).show();
 }
+function check_form() {
+  var errors = [];
+  $.each({ "file" : "an upload file", "name" : "a tournament name", "start" : "a start date", "finish" : "an end date" }, function(key, val) {
+    var input = $("#" + key);
+    if (input.parent().css("display") != "none" && input[0].value == "") errors.push(val)
+  });
+  if (errors.length > 0) alert("Please provide: " + errors.join(", "))
+  return errors.length > 0 ? false : true;
+}
+// Notes:
+// 1. Using .on() instead of .bind() was necessary for the new_upload onsubmit behaviour to work in FireFox and IE.
+// 2. Using coffescript didn't work in FireFox and IE (got the same syntax error as in note 1).
 $(function() {
   $("#start").datepicker({ dateFormat: 'yy-mm-dd' });
   $("#finish").datepicker({ dateFormat: 'yy-mm-dd' });
-  $("#upload_format").bind("change", toggle_file_format).trigger("change")
+  $("#upload_format").on("change", toggle_file_format).trigger("change")
+  $("#new_upload").on("submit", check_form)
 });
