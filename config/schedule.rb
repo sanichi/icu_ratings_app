33ed092d6797b1128fe6444ea86d3bff064bb97e
Mark Orr <mark.j.l.orr@googlemail.com>
2012-07-05 20:39:26 +0100

    Syncing items is so fast there's no reason not to do it daily

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,25 @@
 set :output, "log/cron.log"
 
-every 1.day, at: "3am" do
+every 1.minute do
+  command "cd #{path}; F=tmp/#{@environment}_rating_run; if [ -f $F ]; then mv $F ${F}_; RAILS_ENV=#{@environment} bin/rake rating:run --silent; fi"
+end
+
+every :day, at: "3am" do
   rake "sync:icu_players"
 end
 
-every 1.day, at: "4am" do
+every :day, at: "4am" do
   rake "sync:icu_users"
 end
 
-every :sunday, at: "5am" do
-  rake "sync:irish_fide_players"
-end
-
-every :sunday, at: "6am" do
-  rake "sync:other_fide_players"
-end
-
-every :sunday, at: "7am" do
+every :day, at: "5am" do
   rake "sync:icu_items"
 end
 
-every 1.minute do
-  command "cd #{path}; F=tmp/#{@environment}_rating_run; if [ -f $F ]; then mv $F ${F}_; RAILS_ENV=#{@environment} bin/rake rating:run --silent; fi"
+every :sunday, at: "6am" do
+  rake "sync:irish_fide_players"
+end
+
+every :sunday, at: "7am" do
+  rake "sync:other_fide_players"
 end
