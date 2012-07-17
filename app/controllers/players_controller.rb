6646a21a6cec2846a74ed7aee26240ea0f1ab2ff
Mark Orr <mark.j.l.orr@googlemail.com>
2012-07-17 20:58:56 +0100

    Added link to article about bonuses if there is one

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,5 +3,6 @@ class PlayersController < ApplicationController
     @player = Player.includes(results: [:opponent]).find(params[:id])
     authorize!(:show, @player)
     @tournament = @player.tournament
+    @bonus_article = Article.find_by_headline_and_published("Bonus Rating Points", true)
   end
 end
