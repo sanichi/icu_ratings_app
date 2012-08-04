903796ee7e8bdd12adc235a36488746a77e1ff3a
Mark Orr <mark.j.l.orr@googlemail.com>
2012-08-04 11:49:40 +0100

    Added new old rating for Erin O'Reilly 4126

--- db/migrate/20120604061351_add_old_ratings_from_archive.rb
+++ db/migrate/20120604061351_add_old_ratings_from_archive.rb
@@ -1,12 +1,20 @@
 class AddOldRatingsFromArchive < ActiveRecord::Migration
+  # You can add new records to this migration and rerun it:
+  # RAILS_ENV=production bin/rake db:migrate:redo VERSION=20120604061351
+  RECORDS =
+  [
+    {icu_id: 1074, rating: 1821, games:  7, full: true},
+    {icu_id: 1305, rating: 1190, games: 30, full: true},
+    {icu_id: 2404, rating: 1210, games:  6, full: true},
+    {icu_id: 3758, rating: 1507, games: 12, full: true},
+    {icu_id: 4126, rating:  938, games: 35, full: true},
+  ]
+
   def up
-    OldRating.create!({icu_id: 1074, rating: 1821, games:  7, full: true}, without_protection: true)
-    OldRating.create!({icu_id: 1305, rating: 1190, games: 30, full: true}, without_protection: true)
-    OldRating.create!({icu_id: 2404, rating: 1210, games:  6, full: true}, without_protection: true)
-    OldRating.create!({icu_id: 3758, rating: 1507, games: 12, full: true}, without_protection: true)
+    RECORDS.each { |hash| OldRating.where(icu_id: hash[:icu_id]).first_or_create(hash, without_protection: true) }
   end
 
   def down
-    [1305, 2404, 3758].each { |icu_id| OldRating.find_by_icu_id(icu_id).destroy }
+    RECORDS.each{ |hash| OldRating.find_by_icu_id(hash[:icu_id]).try(:destroy) }
   end
 end
