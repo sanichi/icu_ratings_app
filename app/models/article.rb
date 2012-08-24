8a008a273231dd355ee7d2bcf99f97030c521233
Mark Orr <mark.j.l.orr@googlemail.com>
2012-08-24 22:44:16 +0100

    Fixed article bug and removed beta badge(!!)

--- app/models/article.rb
+++ app/models/article.rb
@@ -45,7 +45,7 @@ class Article < ActiveRecord::Base
     end
     paginate(matches, path, params)
   end
-  
+
   def self.get_by_identity(identity)
     find_by_identity_and_published(identity, true)
   end
@@ -60,7 +60,8 @@ class Article < ActiveRecord::Base
   def normalise_attributes
     self.published = true  if published == "true"
     self.published = false if published == "false"
-    
+    self.identity  = nil   if identity.blank?
+
     # After the upgrade to 3.2.3, spurious newlines started to appear pre-pended to the story.
     # In the release notes for 3.2.3, there's mention of adding a new line after textareas.
     # And for some reason, story is sometimes an Array in spec/requests/articles_spec.rb
