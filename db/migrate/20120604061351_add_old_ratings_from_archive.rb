a173d18dee68c643470bd0f88266e08522600b4f
Mark Orr <mark.j.l.orr@googlemail.com>
2012-10-03 20:56:31 +0100

    Add a new old player - older than the archive in this case

--- db/migrate/20120604061351_add_old_ratings_from_archive.rb
+++ db/migrate/20120604061351_add_old_ratings_from_archive.rb
@@ -3,11 +3,12 @@ class AddOldRatingsFromArchive < ActiveRecord::Migration
   # RAILS_ENV=production bin/rake db:migrate:redo VERSION=20120604061351
   RECORDS =
   [
-    {icu_id: 1074, rating: 1821, games:  7, full: true},
-    {icu_id: 1305, rating: 1190, games: 30, full: true},
-    {icu_id: 2404, rating: 1210, games:  6, full: true},
-    {icu_id: 3758, rating: 1507, games: 12, full: true},
-    {icu_id: 4126, rating:  938, games: 35, full: true},
+    {icu_id:  1074, rating: 1821, games:  7, full: true},
+    {icu_id:  1305, rating: 1190, games: 30, full: true},
+    {icu_id:  2404, rating: 1210, games:  6, full: true},
+    {icu_id:  3758, rating: 1507, games: 12, full: true},
+    {icu_id:  4126, rating:  938, games: 35, full: true},
+    {icu_id: 13382, rating: 1196, games: 80, full: true},
   ]
 
   def up
