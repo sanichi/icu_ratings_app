bee75bd92c1b816f41f789251e56105bbcb230c4
Mark Orr <mark.j.l.orr@googlemail.com>
2013-04-18 19:55:45 +0100

    Don't look for the resource first if the user has no access

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
   def show
+    authorize!(:show, Player)
     @player = Player.includes(results: [:opponent]).find(params[:id])
-    authorize!(:show, @player)
     @tournament = @player.tournament
     @bonus_article = Article.get_by_identity("bonus points")
   end
