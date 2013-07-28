3d6c32a9afd2bfa781ffb8102f4fee6ef628b471
Mark Orr <mark.j.l.orr@googlemail.com>
2013-07-28 15:34:49 +0100

    More tidy up after upgrade to Rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ratings::Application.config.secret_token = APP_CONFIG["secret_token"] # TODO: remove after transition to rails 4
 Ratings::Application.config.secret_key_base = APP_CONFIG["secret_key_base"]
