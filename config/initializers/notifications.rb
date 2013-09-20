a446ef10e11f3ae4b8eb2bdf26a226fad18ad228
Mark Orr <mark.j.l.orr@googlemail.com>
2013-09-20 11:53:27 +0100

    Updated some gems and stopped 406 exceptions being Failures

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,7 +2,7 @@ ActiveSupport::Notifications.subscribe "process_action.action_controller" do |na
   if payload[:exception]
     name = payload[:exception].first
     # Ignore certain exceptions (other candidates would be AbstractController::ActionNotFound and ActionController::RoutingError).
-    unless ["ActiveRecord::RecordNotFound"].include?(name)
+    unless %w[ActiveRecord::RecordNotFound ActionController::UnknownFormat].include?(name)
       # This next is mainly because to_yaml (used to prettify the details) can't handle anonymous modules (e.g. in :request).
       safe = [:controller, :action, :params, :format, :path, :exception]
       details = payload.select{|k,v| safe.include?(k)}.to_yaml
