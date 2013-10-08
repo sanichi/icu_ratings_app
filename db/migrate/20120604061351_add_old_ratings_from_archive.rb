abc9710a75d747fefc49b7ca465b80607ba49972
Mark Orr <mark.j.l.orr@googlemail.com>
2013-10-08 21:03:26 +0100

    Resurrect player from the archive

--- db/migrate/20120604061351_add_old_ratings_from_archive.rb
+++ db/migrate/20120604061351_add_old_ratings_from_archive.rb
@@ -12,6 +12,7 @@ class AddOldRatingsFromArchive < ActiveRecord::Migration
     {icu_id:  1754, rating:  854, games: 20, full: true},  # resurrected from former players
     {icu_id:  2059, rating: 1071, games: 20, full: true},  # old Fred Harte data, number of games guessed
     {icu_id:  2404, rating: 1210, games:  6, full: true},
+    {icu_id:  3928, rating: 1189, games: 13, full: true},  # resurrected from former players
     {icu_id:  3758, rating: 1507, games: 12, full: true},
     {icu_id:  4126, rating:  938, games: 35, full: true},
     {icu_id: 13382, rating: 1196, games: 80, full: true},
