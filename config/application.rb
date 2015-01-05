cdf8a820d56c04fc0aef4e2b48c41bb15b167428
Mark Orr <mark.j.l.orr@googlemail.com>
2015-01-05 16:46:48 +0000

    Updated default mail from address

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Ratings
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # no-reply@icu.ie causes an error at fwd{0,1,2}.hosts.co.uk so for now use this instead which has no problem.
-    config.action_mailer.default_options = { from: "mjo@ratalacha.icu.ie" }
+    # no-reply@icu.ie used to cause an error when we were with register365.
+    config.action_mailer.default_options = { from: "NO-REPLY@icu.ie" }
   end
 end
